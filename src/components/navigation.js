5ce7ed451c62469a6f1c4e4d89ab9d13f97401c5
Richard Taylor Dawson <richard.taylor.dawson@gmail.com>
2019-12-14 22:29:51 -0700

    completing navigation and deleting header

---
+++ src/components/navigation.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Collapse } from "shards-react"
+
+export class Navigation extends React.Component {
+  constructor(props) {
+    super(props)
+    this.toggleNavbar = this.toggleNavbar.bind(this)
+    this.state = { collapseOpen: false }
+    this.activePage = props.currentPage
+  }
+
+  toggleNavbar() {
+    this.setState({
+      ...this.state,
+      ...{
+        collapseOpen: !this.state.collapseOpen
+      }
+    })
+  }
+
+  render() {
+    return (
+      <Navbar sticky="top" type="dark" theme="secondary" expand="md">
+        <NavbarBrand href="/"><img className="m-0" src="/pwa-shields-logo.svg" alt="pwa shields logo" /></NavbarBrand>
+        <NavbarToggler onClick={this.toggleNavbar} />
+
+        <Collapse open={this.state.collapseOpen} navbar>
+          <Nav navbar>
+            <NavItem>
+              <NavLink href="/" active={this.activePage === "series"}>Series</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/create" active={this.activePage === "create"}>Create</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    )
+  }
+}
