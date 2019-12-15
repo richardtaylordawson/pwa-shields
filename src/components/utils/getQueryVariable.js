9f7bf91290557c1fee55875cb0087ee447b6bd41
Richard Taylor Dawson <richard.taylor.dawson@gmail.com>
2019-12-14 22:37:15 -0700

    adding check for window

--- src/components/utils/getQueryVariable.js
+++ src/components/utils/getQueryVariable.js
@@ -4,16 +4,18 @@
 * @return {string} - Returns the string value if the key is found, otherwise false is returned
 */
 export const getQueryVariable = (variable) => {
-  const query = window.location.search.substring(1)
-  const vars = query.split("&")
+  if(typeof window !== "undefined") {
+    const query = window.location.search.substring(1)
+    const vars = query.split("&")
 
-  for (let i = 0; i < vars.length; i++) {
-    const pair = vars[i].split("=")
+    for (let i = 0; i < vars.length; i++) {
+      const pair = vars[i].split("=")
 
-    if(pair[0] === variable) {
-      return pair[1]
+      if(pair[0] === variable) {
+        return pair[1]
+      }
     }
-  }
 
-  return(false)
+    return false
+  }
 }
